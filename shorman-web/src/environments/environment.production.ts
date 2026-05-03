835bde0742edb6cb8e15c19b46e8895d197d9632
KaranamK <kaushik.karanam@l-p-a.com>
2026-05-03 22:35:43 +0200

    setting environment variables

--- shorman-web/src/environments/environment.production.ts
+++ shorman-web/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api'
+  apiUrl: 'https://shorman-api-g0fwefe9cmezgeam.westeurope-01.azurewebsites.net/api', //'http://localhost:5000/api',
 };
